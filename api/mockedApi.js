0ff574665470a44b08febaea27d55545c2dec0f9
Siarhei Plisau <plisov.grey@mail.ru>
2020-08-21 01:13:52 +0300

    delele folder api from root

--- api/mockedApi.js
+++
@@ -1,7 +0,0 @@
-import  { mockedResponse } from './mockedResponse';
-
-export const apiCall = () => {
-    return new Promise( (resolve, regect) => {
-        setTimeout( () => resolve(mockedResponse), 500);
-    })
-}
