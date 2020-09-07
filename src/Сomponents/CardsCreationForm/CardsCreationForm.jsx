df536258e8328b7ff7f37ad65c686e1af9e16c35
Siarhei Plisau <plisov.grey@mail.ru>
2020-09-07 23:51:34 +0300

    add ReactJS_task4

--- src/Сomponents/CardsCreationForm/CardsCreationForm.jsx
+++ src/Сomponents/CardsCreationForm/CardsCreationForm.jsx
@@ -1,22 +1,57 @@
 import React from 'react';
 import styles from './CardsCreationForm.module.scss';
+import FormError from '../FormError';
 
 export default class CardsCreationForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.price = React.createRef();
+    this.title = React.createRef();
+    this.imageUrl = React.createRef();
+    this.about = React.createRef();
+
     this.state = {
       price: '',
       title: '',
       imageUrl: '',
       about: '',
+      errorForm: {
+        price: false,
+        title: false,
+        imageUrl: false,
+        about: false,
+      }, 
     }
     this.handleChangePrice = this.handleChangePrice.bind(this);
     this.handleChangeTitle = this.handleChangeTitle.bind(this);
     this.handleChangeImageUrl = this.handleChangeImageUrl.bind(this);
     this.handleChangeAbout = this.handleChangeAbout.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkError = this.checkError.bind(this);
   }
+
+  checkError() {
+    this.errorLabel = false;
+    const copyState = Object.assign({}, this.state);
+    for (const key in copyState.errorForm) {
+       copyState.errorForm[key] = false;
+    }
+    for (const key in copyState) {
+      if( key === 'errorForm' ) continue;
+      if ( !copyState[key] ) {
+        copyState.errorForm[key] = true;
+        this.setState( state => ({ copyState }));
+        this[key].current.focus();
+        this.errorLabel = true;
+        break;
+      }
+    }
+    return this.errorLabel ? true : false;
+  }
+
   handleSubmit(event) {
+    event.preventDefault();
+    if ( this.checkError() ) return;
     const id = new Date();
     const {price, title, imageUrl, about} = this.state;
     const {addNewCard} = this.props;
@@ -25,9 +60,14 @@ export default class CardsCreationForm extends React.Component {
       price: '',
       title: '',
       imageUrl: '',
-      about: ''
+      about: '',
+      errorForm: {
+        price: false,
+        title: false,
+        imageUrl: false,
+        about: false,
+      },
     }));
-    event.preventDefault();
   }
 
   handleChangePrice(event) {
@@ -48,42 +88,47 @@ export default class CardsCreationForm extends React.Component {
         <label>
           Price:
           <input
+            ref={this.price}
             type="number"
             className={styles.inputPrice}
             value={this.state.price}
             onChange={this.handleChangePrice}
-            required 
-          />
+            />
+          { this.state.errorForm.price && !this.state.price ? <FormError /> : null}
         </label>
 
         <label>
           Title:
           <input
+            ref={this.title}
             type="text"
             className={styles.inputTitle}
             value={this.state.title}
             onChange={this.handleChangeTitle}
-            required 
           />
+          { this.state.errorForm.title && !this.state.title ? <FormError /> : null}
         </label>
 
         <label>
         Image url:
           <input
+            ref={this.imageUrl}
             type="text"
             value={this.state.imageUrl}
             onChange={this.handleChangeImageUrl}
           />
+          {this.state.errorForm.imageUrl && !this.state.imageUrl ? <FormError /> : null}
         </label>
 
         <label>
         Description:
           <input
+            ref={this.about}
             type="text"
             value={this.state.about}
             onChange={this.handleChangeAbout}
-            required 
           />
+          {this.state.errorForm.about && !this.state.about ? <FormError /> : null}
         </label>
         <button type="submit">creat card</button> 
       </form>
